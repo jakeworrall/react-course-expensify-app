9e3f7f66b2c2b0cf140d98436c523de3b3d2cae6
Jake Worrall <jakeworrall@danielles-Air.space48.localnet>
2018-12-11 12:04:24 +0000

    Alter startRemoveExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -29,7 +29,7 @@ export const removeExpense = ({ id } = {}) => ({
     id
 });
 
-export const startRemoveExpense = ({ id }) => {
+export const startRemoveExpense = ({ id } = {}) => {
     return (dispatch) => {
         return database.ref(`expenses/${id}`).remove().then(() => {
             dispatch(removeExpense({ id }));
